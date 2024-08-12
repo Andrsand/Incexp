8b14cdb23e9560b4dd59b7905f1cda7f9f04dbd9
andrsand <angorko@list.ru>
2024-08-12 20:52:40 +0300

    The "TransactiosList.tsx" component has been created.

---
+++ components/TransactionsList.tsx
@@ -0,0 +1,32 @@
+import { TouchableOpacity, View } from "react-native";
+import { Category, Transaction } from "../types";
+
+export default function TransactionList({
+    transactions,
+    categories,
+    deleteTransaction,
+
+}: {
+    categories: Category[];
+    transactions: Transaction[];
+    deleteTransaction: (id: number) => Promise<void>;
+    }) {
+    return (
+        <View>
+            {transactions.map((transaction) => {
+                return (
+                    <TouchableOpacity
+                        key={transaction.id}
+                        activeOpacity={.7}
+                        onLongPress={() => deleteTransaction(transaction.id)}
+                    >
+                        <Text>
+                            {transaction.description} amount: {transaction.amount}
+                        </Text>
+                    </TouchableOpacity>
+                )
+            })}
+        </View>
+    )
+}
+    
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
 import { Text, View } from "react-native";
-import { Category } from '../types';
+import { Category, Transaction } from '../types';
+import { useSQLiteContext } from 'expo-sqlite/next';
+
 
 export default function Home() {
     const [categories, setCategories] = React.useState<Category[]>([]);
+    const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+    
+    const db = useSQLiteContext();
+
+    React.useEffect(() => {
+        db.withTransactionAsync(async () => {
+            await getData();
+        });
+    }, [db]);
+
+    async function getData() {
+        const result = await db.getAllAsync<Transaction>(
+            `SELECT * FROM Transactions ORDER BY date DESC;`
+        );
+        setTransactions(result);
+    }
+
     return (
         <View>
             <Text>Home screen</Text>
