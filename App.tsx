aba91e4a9be82013f05ada4cc114cd9bc20a1337
andrsand <angorko@list.ru>
2024-08-12 06:44:54 +0300

    The database loading functionality has been implemented and the metro.config.js file has been configured

--- App.tsx
+++ App.tsx
@@ -1,7 +1,40 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system'; // импорт из файловой системы
+import { Asset } from 'expo-asset'; // импорт из актива Expo
+
+const loadDatabase = async () => {
+  const dbName = "mySQLiteDB.db";
+  const dbAsset = require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri; // TODO разобраться с этим 
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {                      // если файла не существует...
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+}
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dbLoaded) return (
+    <View style={{ flex: 1 }}>
+      <ActivityIndicator size={"large"} />
+      <Text>Loading Database...</Text>
+    </View>
+  );
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
