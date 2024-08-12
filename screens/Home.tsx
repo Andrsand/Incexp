324cc2e615022da80a04b245c90eb672fd00c2a5
andrsand <angorko@list.ru>
2024-08-12 09:36:56 +0300

    Added types.ts file

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,9 @@
+import * as React from 'react';
 import { Text, View } from "react-native";
+import { Category } from '../types';
 
 export default function Home() {
+    const [categories, setCategories] = React.useState<Category[]>([]);
     return (
         <View>
             <Text>Home screen</Text>
