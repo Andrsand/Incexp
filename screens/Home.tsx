7d483263d142e483b39e7888312240737dabb169
andrsand <angorko@list.ru>
2024-08-14 13:12:56 +0300

    Added TransactionSummary component to Home.tsx

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { Text, View, ScrollView } from "react-native";
-import { Category, Transaction } from '../types';
+import { Category, Transaction, TransactionsByMonth } from '../types';
 import { useSQLiteContext } from 'expo-sqlite/next';
 import TransactionList from '../components/TransactionsList';
+import Card from '../components/ui/Card';
 
 
 export default function Home() {
     const [categories, setCategories] = React.useState<Category[]>([]);
     const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+    const [transactionsByMonth, setTransactionsByMonth] =
+        React.useState<TransactionsByMonth>({
+            totalExpenses: 0,
+            totalIncome: 0,
+        });
+
     
     const db = useSQLiteContext();
 
@@ -33,16 +40,38 @@ export default function Home() {
         db.withTransactionAsync(async () => {
             await db.runAsync(`DELETE FROM Transactions WHERE id = ?;`, [id]);
             await getData();
-        })
+        });
     }
 
     return (
-        <ScrollView contentContainerStyle={{padding: 15, paddingVertical: 170}}>
+        <ScrollView contentContainerStyle={{ padding: 15, paddingVertical: 170 }}>
+            <TransactionSummary
+                totalExpenses={transactionsByMonth.totalExpenses}
+                totalIncome={transactionsByMonth.totalIncome}
+            />
             <TransactionList
                 categories={categories}
                 transactions={transactions}
                 deleteTransaction={deleteTransaction}
-            /> 
+            />
         </ScrollView>
     );
+
+    function TransactionSummary({
+        totalIncome,
+        totalExpenses,
+    }: TransactionsByMonth) {
+        const savings = totalIncome - totalExpenses;
+        const readablePeriod = new Date().toLocaleDateString("default", {
+            month: "long",
+            year: "numeric",
+        });
+        
+        return (
+            <Card>
+                <Text>Summary for {readablePeriod}</Text>
+            </Card>
+        );
+
+    }
 }
