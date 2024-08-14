7e435ca827abce8b836ff10c36287e7a9e5627b5
andrsand <angorko@list.ru>
2024-08-14 12:20:11 +0300

    Added Amount function to TransactionListItem.tsx

--- components/TransactionListItem.tsx
+++ components/TransactionListItem.tsx
@@ -1,5 +1,8 @@
+import { AntDesign } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from "react-native";
 import { Category, Transaction } from "../types";
+import { AutoSizeText, ResizeTextMode } from "react-native-auto-size-text";
+import { categoryColors, categoryEmojies } from "../constants";
 import Card from "./ui/Card";
 
 interface TransactionListItemProps {
@@ -11,12 +14,86 @@ export default function TransactionListItem({
     transaction,
     categoryInfo,
 }: TransactionListItemProps) {
+    const iconName =
+        transaction.type === "Expense" ? "minuscircle" : "pluscircle";
+    const color = transaction.type === "Expense" ? "red" : "green";
+    const categoryColor = categoryColors[categoryInfo?.name ?? "Default"];
+    const emoji = categoryEmojies[categoryInfo?.name ?? "Default"];
     return (
         <Card>
-            <Text>
-            {categoryInfo?.name} amount: {transaction.amount}
-        </Text>
+            <Amount
+                amount={transaction.amount}
+                color={color}
+                iconName={iconName}
+            />
         </Card>
         
-    )
+    );
 }
+
+    function CategoryItem({
+    categoryColor,
+    categoryInfo,
+    emoji,
+    }: {
+    categoryColor: string;
+    categoryInfo: Category | undefined;
+    emoji: string;
+    }) {
+    return (
+        <View
+        style={[
+            styles.categoryContainer,
+            { backgroundColor: categoryColor + "40" },
+        ]}
+        >
+        <Text style={styles.categoryText}>
+            {emoji} {categoryInfo?.name}
+        </Text>
+        </View>
+    );
+    }
+    function Amount({
+        iconName,
+        color,
+        amount,
+    }: {
+        iconName: "minuscircle" | "pluscircle";
+        color: string;
+        amount: number;
+    }) {
+    return (
+        <View style={styles.row}>
+        <AntDesign name={iconName} size={18} color={color} />
+        <AutoSizeText
+            fontSize={32}
+            mode={ResizeTextMode.max_lines}
+            numberOfLines={1}
+            style={[styles.amount, { maxWidth: "80%" }]}
+        >
+            ${amount}
+        </AutoSizeText>
+        </View>
+    );
+    }
+
+    const styles = StyleSheet.create({
+  amount: {
+    fontSize: 32,
+    fontWeight: "800",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+  },
+  categoryContainer: {
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    alignSelf: "flex-start",
+  },
+  categoryText: {
+    fontSize: 12,
+  },
+});
